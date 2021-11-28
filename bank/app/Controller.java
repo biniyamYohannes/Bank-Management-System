15ed19afae8148d39646e1a9bf2ea3a3227dab34
Biniyam Yohannes <biniyamyohannes.by@gmail.com>
2021-11-28 15:25:36 -0700

    removed java bank code

--- bank/app/Controller.java
+++
@@ -1,4 +0,0 @@
-package bank.app;
-
-public class Controller {
-}
--- bank/denverBank/Checking.java
+++
@@ -1,9 +0,0 @@
-package bank.denverBank;
-
-public class Checking extends Account {
-
-
-    public Checking(String accountId, int accountType, float balance) {
-        super(accountId, 1, balance);
-    }
-}
