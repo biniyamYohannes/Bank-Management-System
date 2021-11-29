9378d80f1583cb7b35800b070ccf2ecc46ef4322
Biniyam Yohannes <biniyamyohannes.by@gmail.com>
2021-11-28 20:12:06 -0700

    added bank classes and some db code

--- python/bank.py
+++ python/bank.py
@@ -2,6 +2,7 @@
 """Implements bank classes Bank, Customer, Account, and Transaction."""
 
 import datetime
+import mysql.connector
 
 # Class Transaction
 class Transaction:
@@ -25,6 +26,19 @@ class Transaction:
         """String representation of a Transaction object."""
         return f'Timestamp: {self.timestamp.year}/{self.timestamp.month}/{self.timestamp.day} - {self.timestamp.hour}:{self.timestamp.minute}:{self.timestamp.second} |' \
                f' Amount: {self.amount:.2f}'
+
+    # def load(self):
+    #     """Load a transaction from the database."""
+    #     my_db = mysql.connector.connect(host=Bank.DB['hostname'], port=Bank.DB['port'], user=Bank.DB['user'], password=Bank.DB['passwd'], database=Bank.DB['db'])
+    #     cursor = my_db.cursor()
+    #     cursor.execute('SELECT trans_id, trans_account_id, trans_amount, trans_created WHERE trans_id = %s', ())
+    #
+    # @staticmethod
+    # def store():
+    #     my_db = mysql.connector.connect(host=Bank.DB['hostname'], port=Bank.DB['port'], user=Bank.DB['user'], password=Bank.DB['passwd'], database=Bank.DB['db'])
+    #     cursor = my_db.cursor()
+    #     cursor.execute('SELECT trans_id, trans_account_id, trans_amount, trans_created WHERE trans_id = %s', ())
+
 
 # ####################################################################################
 
@@ -70,12 +84,35 @@ class Account:
 # Class Customer
 class Customer:
     """Class that will represent a bank's customer."""
-    def __init__(self, fname: str, lname: str, email: str):
-        """Customer constructor."""
-        self.__fname = fname
-        self.__lname = lname
-        self.__email = email
-        self.__accounts = []
+    def __new__(cls, email: str, password: str):
+        """Create an instance of the customer class if there is a password match."""
+        try:
+            my_db = mysql.connector.connect(host=Bank.DB['hostname'], port=Bank.DB['port'], user=Bank.DB['user'], password=Bank.DB['passwd'], database=Bank.DB['db'])
+            cursor = my_db.cursor()
+            cursor.execute('SELECT cust_fname, cust_lname, cust_pass FROM customer WHERE cust_email = %s', (email,))
+            for row in cursor.fetchall():
+                if row[2] != password:
+                    print('Password validation failed.')
+                    return ''
+                else:
+                    # create a new instance and set attributes on it
+                    instance = super().__new__(cls)  # empty instance
+                    instance.__fname = row[0]
+                    instance.__lname = row[1]
+                    instance.__email = email
+                    instance.__accounts = []
+                    return instance
+        except:
+            print('Something went wrong trying to reach the DB.')
+            return ''
+
+    # def __init__(self, email: str, password: str):
+    #     """Customer constructor."""
+    #     self.__email = email
+    #     self.__fname = None
+    #     self.__lname = None
+    #     self.__accounts = []
+    #     self.load(password)
 
     def add_account(self, id: str, type: int, balance: float):
         """Add an account to the list of customer's accounts."""
@@ -105,11 +142,34 @@ class Customer:
         """String representation of a Customer object."""
         return f'Name: {self.fname} {self.lname} | Email {self.email} | Accounts: {len(self.accounts)}'
 
+    def load(self, password: str):
+        """Load a transaction from the database."""
+        try:
+            my_db = mysql.connector.connect(host=Bank.DB['hostname'], port=Bank.DB['port'], user=Bank.DB['user'], password=Bank.DB['passwd'], database=Bank.DB['db'])
+            cursor = my_db.cursor()
+            cursor.execute('SELECT cust_fname, cust_lname, cust_pass FROM customer WHERE cust_email = %s', (self.email,))
+            for row in cursor.fetchall():
+                if row[2] == password:
+                    self.__fname = row[0]
+                    self.__lname = row[1]
+            cursor.close()
+            my_db.close()
+        except:
+            print('Password validation failed.')
+
+    # @staticmethod
+    # def store():
+    #     my_db = mysql.connector.connect(host=Bank.DB['hostname'], port=Bank.DB['port'], user=Bank.DB['user'], password=Bank.DB['passwd'], database=Bank.DB['db'])
+    #     cursor = my_db.cursor()
+    #     cursor.execute('SELECT trans_id, trans_account_id, trans_amount, trans_created WHERE trans_id = %s', (self.email))
+
 
 # ####################################################################################
 
 # Class Bank
 class Bank:
+    DB = {'hostname': 'localhost', 'port': 3306, 'user': 'root', 'passwd': 'password', 'db': 'test'}
+
     """Class that will represent a bank."""
     def __init__(self, name: str):
         """Bank constructor."""
@@ -140,21 +200,13 @@ class Bank:
         """String representation of a Bank object."""
         return f'Name: {self.name} | Customers: {len(self.customers)}'
 
+# ####################################################################################
 
-
-
+# Test code
 if __name__ == "__main__":
-    transaction = Transaction(datetime.datetime.now(), 1000.)
-    print(transaction)
-    account = Account('000000', 1, 1000)
-    print(account)
-    account.add_transaction(datetime.datetime.now(), 1000.)
-    print(account)
-    customer = Customer('Biniyam', 'Yohannes', 'biniyam.yohannes@ucdenver.edu')
-    print(customer)
-    customer.add_account('000000', 1, 1000)
-    print(customer)
-    bank = Bank("Test Bank")
-    print(bank)
-    bank.addCustomer('Biniyam', 'Yohannes', 'biniyam.yohannes@ucdenver.edu')
-    print(bank)
+    customer1 = Customer('biniyam.yohannes@ucdenver.edu', 'password')
+    print(customer1) # Name: Biniyam Yohannes | Email biniyam.yohannes@ucdenver.edu | Accounts: 0
+    customer2 = Customer('biniyam.yohannes@ucdenver.edu', 'invalid_password')
+    print(customer2) # Password validation failed.
+    if not customer2:
+        print('Customer 2 returned an empty string')
