576108db684f723b8baa9a21a83661f2dfb289f3
anle9650 <andy.le@ucdenver.edu>
2021-12-03 22:03:25 -0700

    created main checking account scene

--- BankGUI/src/finalproject/application/Controller.java
+++ BankGUI/src/finalproject/application/Controller.java
@@ -3,6 +3,7 @@ package finalproject.application;
 import finalproject.application.models.Account;
 import finalproject.application.models.CreditAccount;
 import finalproject.application.models.SavingsAccount;
+import finalproject.application.models.Transaction;
 import finalproject.client.Client;
 import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
@@ -17,6 +18,7 @@ import java.io.IOException;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Controller {
     // client and account data for current session
@@ -38,21 +40,54 @@ public class Controller {
     public MenuButton menuAccountType;
     public TextField txtSSN;
     public PasswordField txtPassword;
+    public Label labelFirstName;
+    public Label labelAccount;
     public ListView<Account> listAccounts;
+    public ListView<Transaction> listRecentTransactions;
     public Button btnCreateAccount;
     public Button btnLogin;
     public Button btnAddAccount;
+    public Button btnBack;
     public Button btnSelectAccount;
+    public Button btnWithdraw;
+    public Button btnDeposit;
+    public Button btnTransactions;
 
     public Controller() {
         if (!client.isConnected())
             client.connect();
 
+        this.labelFirstName = new Label();
+        this.labelAccount = new Label();
         this.listAccounts = new ListView<>();
+        this.listRecentTransactions = new ListView<>();
     }
 
     public void initialize() {
+        // initialize the GUI for user's first name and list of accounts
+        this.labelFirstName.setText(firstName);
         this.listAccounts.setItems(FXCollections.observableArrayList(accounts));
+
+        // return here if a user account is not selected yet
+        if (currentAccount == null)
+            return;
+
+        // initialize GUI component for account name
+        this.labelAccount.setText(currentAccount.toString());
+
+        // get the current account's transactions
+        ArrayList<Transaction> transactions = currentAccount.getTransactions();
+
+        // get the current account's most recent 7 transactions
+        List<Transaction> recentTransactions;
+        try {
+            recentTransactions = transactions.subList(0, 7);
+        } catch(IndexOutOfBoundsException e) {
+            recentTransactions = transactions;
+        }
+
+        // initialize GUI component for recent transactions
+        this.listRecentTransactions.setItems(FXCollections.observableArrayList(recentTransactions));
     }
 
 // Scene handlers ------------------------------------------------------------------------------------------------------
@@ -64,21 +99,25 @@ public class Controller {
     }
 
     // Loads a scene from an FXML file.
-    private void loadScene(Stage stage, String fxml) throws IOException {
+    private void loadScene(ActionEvent actionEvent, String fxml) throws IOException {
+        Stage stage = this.getStage(actionEvent);
         Parent root = FXMLLoader.load(getClass().getResource("views/" + fxml));
         stage.setScene(new Scene(root, 720, 480));
     }
 
+    // Loads the login scene.
+    public void loadLogin(ActionEvent actionEvent) throws IOException {
+        this.loadScene(actionEvent, "login.fxml");
+    }
+
     // Loads the create customer scene.
     public void loadCreateCustomer(ActionEvent actionEvent) throws IOException {
-        Stage stage = getStage(actionEvent);
-        this.loadScene(stage, "create_customer.fxml");
+        this.loadScene(actionEvent, "create_customer.fxml");
     }
 
     // Loads the add account scene.
     public void loadAddAccount(ActionEvent actionEvent) throws IOException {
-        Stage stage = getStage(actionEvent);
-        this.loadScene(stage, "add_account.fxml");
+        this.loadScene(actionEvent, "add_account.fxml");
     }
 
     // Sets the user-selected account type as "savings".
@@ -101,21 +140,32 @@ public class Controller {
 
     // Loads the account selection scene.
     public void loadAccountSelection(ActionEvent actionEvent) throws IOException {
-        Stage stage = getStage(actionEvent);
-        this.loadScene(stage, "account_selection.fxml");
+        this.loadScene(actionEvent, "account_selection.fxml");
     }
 
-    // Handles the event that the user clicks on an account in the account list by setting it as the current account
+    // Handles the event that the user clicks on an account in the account list by setting it as the current account.
     public void selectAccount(MouseEvent mouseEvent) {
         currentAccount = this.listAccounts.getSelectionModel().getSelectedItem();
     }
 
     // Loads the main account scene.
     public void loadAccountMain(ActionEvent actionEvent) throws IOException {
-        Stage stage = getStage(actionEvent);
+        this.loadScene(actionEvent, currentAccount.getType() + "_main.fxml");
+    }
 
-        // load the main account scene based on the current account's type
-        this.loadScene(stage, currentAccount.getType() + "_main.fxml");
+    // Loads the account withdrawal scene.
+    public void loadWithdraw(ActionEvent actionEvent) throws IOException {
+        this.loadScene(actionEvent, "account_withdraw.fxml");
+    }
+
+    // Loads the account deposit scene.
+    public void loadDeposit(ActionEvent actionEvent) throws IOException {
+        this.loadScene(actionEvent, "account_deposit.fxml");
+    }
+
+    // Loads the account transactions scene.
+    public void loadTransactions(ActionEvent actionEvent) throws IOException {
+        this.loadScene(actionEvent, "account_transactions.fxml");
     }
 
 // Client-server communication handlers --------------------------------------------------------------------------------
@@ -150,8 +200,6 @@ public class Controller {
 
     // Attempts to log into an account on the server with the provided credentials.
     public void login(ActionEvent actionEvent) throws IOException {
-        // Request: login | email | password
-        // Response: {success/fail} | fname | lname | email
 
         // get the values inputted into the text fields
         String email = txtEmail.getText();
@@ -175,22 +223,27 @@ public class Controller {
 
                 // load the account selection scene
                 this.loadAccountSelection(actionEvent);
-
-                // display success alert
-//                this.successAlert("Login successful.");
                 break;
 
             case "fail":
-                // display failure alert with message from server
                 this.failAlert(respArgs[1]);
+
+//                firstName= "Andy";
+//                lastName = "Le";
+//                Account checking = new Account("1", "checking", 200);
+//                Transaction trans1 = new Transaction(LocalDate.of(2017, 1, 13), 100);
+//                Transaction trans2 = new Transaction(LocalDate.of(2017, 2, 13), -50);
+//                checking.addTransaction(trans1);
+//                checking.addTransaction(trans2);
+//                accounts.add(checking);
+//                this.loadAccountSelection(actionEvent);
+
                 break;
         }
     }
 
     // Fetches and returns the user's account IDs from the server.
     private String[] getAccountIDs() {
-        // Request: customer | get | all
-        // Response: {success/fail} | accountID1 | accountID2| accountID3 | ...
 
         String[] accountIDs;
 
@@ -207,12 +260,10 @@ public class Controller {
                 break;
 
             case "fail":
-                // return an empty array
                 accountIDs = new String[]{};
-
-                // display failure alert with message from server
                 this.failAlert(respArgs[1]);
                 break;
+
             default:
                 throw new IllegalStateException("Unexpected value: " + respArgs[0]);
         }
@@ -222,8 +273,6 @@ public class Controller {
 
     // Requests an account's information from the server, returns an Account object containing the account info.
     private Account getAccount(String accountID) {
-        // Request: account | get | [accountId]
-        // Response: {success/fail} | [Balance] | [AccountType] | {Saving: [interestRate] | Credit Card: [CreditLimit]}
 
         // send create customer request to the server and receive server's response.
         String cmd = String.format("account|get|%s", accountID);
@@ -265,6 +314,7 @@ public class Controller {
 
     // Fetches and stores the current user's accounts from the server.
     private void getAccounts() {
+
         // get the user's account IDs
         String[] accountIDs = this.getAccountIDs();
 
@@ -312,7 +362,6 @@ public class Controller {
                 break;
 
             case "fail":
-                // display failure alert with message from server
                 this.failAlert(respArgs[1]);
                 break;
         }
@@ -351,7 +400,6 @@ public class Controller {
                 break;
 
             case "fail":
-                // display failure alert with message from server
                 this.failAlert(respArgs[1]);
                 break;
         }
