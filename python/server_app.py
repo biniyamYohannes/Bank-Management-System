481038d4c6d90d913823b639cdc9d192cf1080cf
EliMounjim <93162668+EliMounjim@users.noreply.github.com>
2021-12-07 11:44:21 -0700

    Update server_app.py

--- python/server_app.py
+++ python/server_app.py
@@ -1,6 +1,6 @@
-from server import Server
+from server import Server, MultiServer
 
 if __name__ == "__main__":
-    server = Server('First Bank', 'localhost', 10000, 5)
+    server = MultiServer('First Bank', 'localhost', 10000, 5)
 
     server.run()
