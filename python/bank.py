9c2ce286f7f8c72596c2a66b634a20510772d5cf
Biniyam Yohannes <biniyamyohannes.by@gmail.com>
2021-12-06 20:04:10 -0700

    implemented transactions

--- python/bank.py
+++ python/bank.py
@@ -7,10 +7,11 @@ import mysql.connector
 # Class Transaction
 class Transaction:
     """Class that willl represent an account transaction (deposit/withdrawal). """
-    def __init__(self, timestamp: datetime.datetime, amount: float):
+    def __init__(self, timestamp: datetime.datetime, amount: float, trans_id: int):
         """Transaction constructor."""
         self.__timestamp = timestamp
         self.__amount = amount
+        self.__trans_id = trans_id
 
     @property
     def timestamp(self):
@@ -22,11 +23,15 @@ class Transaction:
         """Get transaction's amount attribute."""
         return self.__amount
 
+    @property
+    def trans_id(self):
+        """Get transaction's id attribute."""
+        return self.__trans_id
+
     def __str__(self):
         """String representation of a Transaction object."""
-        return f'Timestamp: {self.timestamp.year}/{self.timestamp.month}/{self.timestamp.day} - \
-                            {self.timestamp.hour}: {self.timestamp.minute}:{self.timestamp.second} | \
-                            Amount: {self.amount:.2f}'
+        return f'{self.timestamp.year}-{self.timestamp.month}-{self.timestamp.day} {self.timestamp.hour}:' \
+               f'{self.timestamp.minute}:{self.timestamp.second},{self.amount:.2f}'
 
 # ##################################################################################################
 
@@ -59,12 +64,39 @@ class Account:
             print('Account:__new__: Something went wrong trying to reach the DB.')
             return None
 
-    def add_transaction(self, timestamp: datetime.datetime, amount: float):
+    def add_transaction(self, timestamp: datetime.datetime, amount: float, trans_id: int):
         """Add a transaction to the account's list of transactions."""
-        self.transactions.append(Transaction(timestamp, amount))
+        self.transactions.append(Transaction(timestamp, amount, trans_id))
+
+    def init_transactions(self):
+        """Initialize the transactions list to an empty state."""
+        self.transactions = []
+
+    def load_all_transactions(self):
+        """Load all accounts from the database."""
+        self.init_transactions()
+        try:
+            my_db = mysql.connector.connect(host=Bank.DB['hostname'],port=Bank.DB['port'],
+                                            user=Bank.DB['user'],password=Bank.DB['passwd'],
+                                            database=Bank.DB['db'])
+            print('SELECT trans_created, trans_amount, trans_account_id, trans_id FROM transaction WHERE trans_account_id = {}'.format(self.acc_id))
+            cursor = my_db.cursor()
+            cursor.execute('SELECT trans_created, trans_amount, trans_account_id, trans_id '
+                           'FROM transaction '
+                           'WHERE trans_account_id = %s', (self.acc_id,))
+
+            for row in cursor.fetchall():
+                self.add_transaction(row[0], row[1], row[3])    # populate the transactions list with Transaction objects
+
+            cursor.close()
+            my_db.close()
+            if not self.transactions:
+                raise ValueError('Failed to retrieve account data for the requested customer.')
+        except:
+            print('Something went wrong when retrieving transactions from the database.')
 
     @property
-    def id(self):
+    def acc_id(self):
         """Get Account id."""
         return self.__acc_id
 
@@ -86,6 +118,11 @@ class Account:
     @property
     def transactions(self):
         return self.__transactions
+
+    @transactions.setter
+    def transactions(self, transaction_list):
+        self.__transactions = transaction_list
+
 
     def __str__(self):
         """String representation of an Account object."""
@@ -152,19 +189,32 @@ class Customer:
     def get_account_ids(self) -> list:
         """Reload all accounts and return their account ids as strings."""
         self.load_all_accounts()
-        return [str(account.id) for account in self.accounts]
+        return [str(account.acc_id) for account in self.accounts]
 
-    def get_account(self, id: str):
+    def get_account(self, account_id: str):
         """Find and return an account based on account id."""
         self.load_all_accounts()
-        return self.find_account(id)
+        return self.find_account(account_id)
 
-    def find_account(self, id: str):
+    def find_account(self, account_id: str):
         # Find account in the current account list by id, if not found return None
         for account in self.accounts:
-            if str(account.id) == id:
+            if str(account.acc_id) == account_id:
                 return account
         return None
+
+    def init_accounts(self):
+        """Initialize accounts list to an empty state."""
+        self.accounts = []
+
+    def get_transactions(self, account_id: str):
+        """Return all transactions for a given account."""
+        acc = self.get_account(account_id)
+        if acc:
+            acc.load_all_transactions()
+            return [f"{str(transaction)}" for transaction in acc.transactions]
+        else:
+            return None
 
     @property
     def fname(self):
@@ -189,9 +239,6 @@ class Customer:
     @accounts.setter
     def accounts(self, account_list):
         self.__accounts = account_list
-
-    def init_accounts(self):
-        self.accounts = []
 
     def __str__(self):
         """String representation of a Customer object."""
